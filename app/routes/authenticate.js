e544b0bbb3bb221cc020b1e2ccd0ab5e4e1db25b
Ayman <ammariayman95@gmail.com>
2019-11-09 13:24:19 +0100

    test

--- app/routes/authenticate.js
+++ app/routes/authenticate.js
@@ -10,8 +10,8 @@ const jwtExpirySeconds = 60;
 // call postgres to verify request's information
 // if OK, creates a jwt and stores it in a cookie, 401 otherwise
 async function authenticateUser(req, res, next) {
-  const { login } = req.body;
-  const { pwd } = req.body;
+  const { login } = req.body.login;
+  const { pwd } = req.body.password;
   debug(req.body);
   //const hashedPwd = await hashing.hashPassword(pwd);
   debug(`authenticate_user(): attempt from "${login}" with password "${pwd}"`);
