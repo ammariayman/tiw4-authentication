2a4f926ec3724d68f8ed57eaf09d20fbd1b5102c
Ayman <ammariayman95@gmail.com>
2019-11-09 15:02:19 +0100

    test

--- app/routes/authenticate.js
+++ app/routes/authenticate.js
@@ -22,7 +22,7 @@ async function authenticateUser(req, res, next) {
 
     // const { password: loginPassword }  = await db.selectUser(login);
     // const user = array();
-    user = await db.selectUser(login);
+    const user = await db.selectUser(login);
     debug(`${user}`);
     // const userPassword = user.password;
     debug(`hashing.comparePassword(): attempt with "${password}" and userPassword "${user[0].password}"`);
